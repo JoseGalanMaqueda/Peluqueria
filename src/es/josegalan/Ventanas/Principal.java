25001f913b32c9803702a3e06162ff765e84bcb1
joseantoniogalan <joseantoniogalan@JoseAntonioGalan>
2021-03-03 17:33:44 +0100

    Cambio a Actualizar Citas en el Boton

--- src/es/josegalan/Ventanas/Principal.java
+++ src/es/josegalan/Ventanas/Principal.java
@@ -29,7 +29,7 @@ public class Principal implements WindowListener, ActionListener
 	Frame ventanaPrincipal = new Frame("Peluqueria Forever Young");
 	Label lblProximasCitas = new Label("Proximas Citas:");
 	TextArea txaCitasHoy = new TextArea(16,54);
-	Button btnActualizar = new Button("Actualizar");
+	Button btnActualizar = new Button("Actualizar Citas");
 	Color clFondo = new Color(204,229,255);
 
 	//===================================== MENU PRINCIPAL =====================================
